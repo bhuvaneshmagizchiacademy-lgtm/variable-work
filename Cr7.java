8bf4380a23a93f6d3217c0ff40c52e87674f8e02
bhuvaneshmagizchiacademy-lgtm <bhuvaneshmagizchiacademy@gmail.com>
2026-07-02 19:37:14 +0530

    variable work

---
+++ Cr7.java
@@ -0,0 +1,14 @@
+class Cr7 {
+    public static void main(String[] args) {
+
+        String name = "Hari";
+        int age = 17;
+        char grade = 'B';
+        boolean passed = true;
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Grade : " + grade);
+        System.out.println("Passed : " + passed);
+    }
+}
